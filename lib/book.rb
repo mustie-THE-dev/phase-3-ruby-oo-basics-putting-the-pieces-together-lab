22e5c121c5ef9e2241ad15bd6f998be5dba64db0
Simon kamau <simon.kamau@student.moringaschool.com>
2022-08-19 15:07:14 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+    attr_reader :title
+    attr_accessor :genre, :page_count, :author
 
+    def initialize(title="And Then There Were None")
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
